4b168722116243d2b47e63f500ca4e4a38d5b33f
Shartechs <shartechs@gmail.com>
2023-07-03 19:41:34 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello Vite</h1>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
     </>
   );
 }
