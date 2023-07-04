1af9774f53c0bce606015cb24c63a6da2f7cf118
Shartechs <shartechs@gmail.com>
2023-07-04 15:36:39 +0200

    added storeItems Store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  );
 };
