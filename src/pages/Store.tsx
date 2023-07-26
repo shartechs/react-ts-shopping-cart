8547cb74f96b05e18763bddb849b6fa50a3b46ff
Shartechs <shartechs@gmail.com>
2023-07-26 23:51:13 +0200

    added storeItem component and formatter utility

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,5 @@
 import storeItems from "../data/items.json";
+import StoreItem from "../components/StoreItem";
 import { Row, Col } from "react-bootstrap";
 
 export const Store = () => {
@@ -7,7 +8,9 @@ export const Store = () => {
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => (
-          <Col>{JSON.stringify(item)}</Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
       </Row>
     </>
